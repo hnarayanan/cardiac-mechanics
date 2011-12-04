121b6db5ec87d1f93b1ec481a6982ace5892bcb9
Harish Narayanan <harish@simula.no>
2011-12-04 09:15:01 +0100

    Updated the algebra to calculate different shear stresses

--- algebra.py
+++ algebra.py
@@ -2,51 +2,11 @@ from sympy import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-print("-"*72)
-print("Passive response of the myocardium")
-print("-"*72)
-
 # Declare useful symbols
 I, B, C, gamma, f0, s0, n0 = symbols("I, B, C, gamma, f0, s0, n0")
-a, b, a_f, b_f, a_s, b_s, a_fs, b_fs = \
-    symbols("a, b, a_f, b_f, a_s, b_s, a_fs, b_fs")
 sigma, p = symbols("sigma, p")
 
-# Identity Matrix
-I = Matrix([[1, 0, 0],
-            [0, 1, 0],
-            [0, 0, 1]])
-
-# Deformation gradient
-F = Matrix([[1, gamma, 0],
-            [0, 1, gamma],
-            [0, 0, 1]])
-
-# Cauchy-Green tensors
-B = F*F.T
-C = F.T*F
-
-# Principle isotropic invariants
-I1 = B.trace()
-I2 = Rational(1, 2)*(I1*I1 - (B*B).trace())
-I3 = B.det()
-
-# Reference fibre, sheet and sheet-normal directions
-f0 = Matrix([1, 0, 0])
-s0 = Matrix([0, 1, 0])
-
-# Anisotropic (quasi) invariants
-I4_f = (f0.T*C*f0)[0]
-I4_s = (s0.T*C*s0)[0]
-I8_fs = (f0.T*C*s0)[0]
-
-# Current fibre, sheet and sheet-normal directions
-f = F*f0
-s = F*s0
-
-# Material parameters
-# Figure 7
+# Material parameters for Figure 7
 a    =  0.059 #kPa
 b    =  8.023
 a_f  = 18.472 #kPa
@@ -57,16 +17,91 @@ a_fs =  0.216 #kPa
 b_fs = 11.436
 p    =  a     #kPa
 
-# Cauchy stress
-sigma =   a*exp(b*(I1 - 3))*B - p*I \
+def sigma(F):
+    # Cauchy-Green tensors
+    B = F*F.T
+    C = F.T*F
+
+    # Principle isotropic invariants
+    I1 = B.trace()
+    I2 = Rational(1, 2)*(I1*I1 - (B*B).trace())
+    I3 = B.det()
+
+    # Reference fibre, sheet and sheet-normal directions
+    f0 = Matrix([1, 0, 0])
+    s0 = Matrix([0, 1, 0])
+
+    # Anisotropic (quasi) invariants
+    I4_f = (f0.T*C*f0)[0]
+    I4_s = (s0.T*C*s0)[0]
+    I8_fs = (f0.T*C*s0)[0]
+
+    # Current fibre, sheet and sheet-normal directions
+    f = F*f0
+    s = F*s0
+
+    # Cauchy stress
+    sigma =   a*exp(b*(I1 - 3))*B - p*I \
         + 2*a_f*(I4_f - 1)*exp(b_f*(I4_f - 1)**2)*(f*f.T) \
         + 2*a_s*(I4_s - 1)*exp(b_s*(I4_s - 1)**2)*(s*s.T) \
         + a_fs*I8_fs*exp(b_fs*I8_fs**2)*(f*s.T + s*f.T)
 
-gammas = np.arange(0, 0.51, 0.01)
-sigma0 = []
-for _gamma in gammas:
-    sigma0.append(sigma[0].subs({gamma:_gamma}))
+    return(sigma)
+#    return(I3*sigma*F.inv().T)
 
-plt.plot(gammas, sigma0)
+# Identity Matrix
+I = Matrix([[1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1]])
+
+# Deformation gradients and corresponding stresses
+F_fs = Matrix([[1, 0, 0],
+               [gamma, 1, 0],
+               [0, 0, 1]])
+sigma_fs = sigma(F_fs)
+
+F_fn = Matrix([[1, 0, 0],
+               [0, 1, 0],
+               [gamma, 0, 1]])
+sigma_fn = sigma(F_fn)
+
+F_sf = F_fs.T
+sigma_sf = sigma(F_sf)
+
+F_sn = Matrix([[1, 0, 0],
+               [0, 1, 0],
+               [0, gamma, 1]])
+sigma_sn = sigma(F_sn)
+
+F_nf = F_fn.T
+sigma_nf = sigma(F_nf)
+
+F_ns = F_sn.T
+sigma_ns = sigma(F_ns)
+
+gammas = np.arange(0, 0.51, 0.01)
+sigma_fs_plot = []
+sigma_fn_plot = []
+sigma_sf_plot = []
+sigma_sn_plot = []
+sigma_nf_plot = []
+sigma_ns_plot = []
+
+for _gamma in gammas:
+    sigma_fs_plot.append(sigma_fs[1].subs({gamma:_gamma}))
+    sigma_fn_plot.append(sigma_fn[6].subs({gamma:_gamma}))
+    sigma_sf_plot.append(sigma_sf[3].subs({gamma:_gamma}))
+    sigma_sn_plot.append(sigma_sn[5].subs({gamma:_gamma}))
+    sigma_nf_plot.append(sigma_nf[2].subs({gamma:_gamma}))
+    sigma_ns_plot.append(sigma_ns[7].subs({gamma:_gamma}))
+
+plt.plot(gammas, sigma_fs_plot, label='fs')
+plt.plot(gammas, sigma_fn_plot, label='fn')
+plt.plot(gammas, sigma_sf_plot, label='sf')
+plt.plot(gammas, sigma_sn_plot, label='sn')
+plt.plot(gammas, sigma_nf_plot, label='nf')
+plt.plot(gammas, sigma_ns_plot, label='ns')
+plt.legend(loc=(0.75, 0.6))
+plt.xlabel("Amount of shear")
+plt.ylabel("Shear stress (kPa)")
 plt.show()
