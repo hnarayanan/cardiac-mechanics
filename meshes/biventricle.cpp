ca60ee7ab061a3ccfef32d88ddfbcd907b400d5a
Harish Narayanan <harish@simula.no>
2011-12-24 09:40:48 +0100

    Now load mesh parameters from a file

--- meshes/biventricle.cpp
+++ meshes/biventricle.cpp
@@ -8,6 +8,11 @@
 #include <CGAL/Mesh_domain_with_polyline_features_3.h>
 #include <CGAL/make_mesh_3.h>
 
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <boost/lexical_cast.hpp>
+
 // Kernel
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 
@@ -33,51 +38,79 @@ typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
 // To avoid verbose function and named parameters call
 using namespace CGAL::parameters;
 
-// Parameters defining the ventricular geometry
+// Declare parameters defining the ventricular geometry (defined in parameters.ini)
+
+// General
+double h;
 
 // Left ventricle
-// Origin
-double l_o_x = 0.0;
-double l_o_y = 0.0;
-double l_o_z = 0.0;
-
+// Center
+double l_c_x, l_c_y, l_c_z;
 // Major endocardium axes
-double l_a_endo = 2.5;       // z
-double l_b_endo = 1.1;       // x
-double l_c_endo = l_b_endo;  // y
+double l_a_endo, l_a_trunc, l_b_endo, l_c_endo;
 
 // Myocardium thicknesses
-double l_a_trunc = 1.4;
-double l_t_equator_b = 0.6;
-double l_t_equator_c = l_t_equator_b;
-double l_t_apex = 0.3;
+double l_t_equator_b, l_t_equator_c, l_t_apex;
 
 // Major epicardium axes
-double l_a_epi = l_a_endo + l_t_apex;
-double l_b_epi = l_b_endo + l_t_equator_b;
-double l_c_epi = l_c_endo + l_t_equator_c;
+double l_a_epi, l_b_epi, l_c_epi;
 
 //Right ventricle
+// Center
+double r_c_x, r_c_y, r_c_z;
+
 // Major endocardium axes
-double r_a_endo = 2.2;
-double r_b_endo = 1.4;
-double r_c_endo = r_b_endo;
+double r_a_endo, r_a_trunc, r_b_endo, r_c_endo;
 
 // Myocardium thicknesses
-double r_a_trunc = 0.75;
-double r_t_equator_b = 0.25;
-double r_t_equator_c = r_t_equator_b;
-double r_t_apex = 0.25;
-
-//Origin
-double r_o_x = l_o_x - 1.0;
-double r_o_y = l_o_y + 0.0;
-double r_o_z = l_o_z + l_a_trunc - r_a_trunc;
+double r_t_equator_b, r_t_equator_c, r_t_apex;
 
 // Major epicardium axes
-double r_a_epi = r_a_endo + r_t_apex;
-double r_b_epi = r_b_endo + r_t_equator_b;
-double r_c_epi = r_c_endo + r_t_equator_c;
+double r_a_epi, r_b_epi, r_c_epi;
+
+void load_parameters()
+{
+
+    boost::property_tree::ptree pt;
+    boost::property_tree::ini_parser::read_ini("parameters.ini", pt);
+
+    h = boost::lexical_cast<double>(pt.get<std::string>("general.h"));
+
+    l_c_x = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.center_x"));
+    l_c_y = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.center_y"));
+    l_c_z = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.center_z"));
+
+    l_a_endo = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.a_endo"));
+    l_a_trunc = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.a_trunc"));
+    l_b_endo = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.b_endo"));
+    l_c_endo = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.c_endo"));
+
+    l_t_equator_b = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.t_equator_b"));
+    l_t_equator_c = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.t_equator_c"));
+    l_t_apex = boost::lexical_cast<double>(pt.get<std::string>("left_ventricle.t_apex"));
+
+    l_a_epi = l_a_endo + l_t_apex;
+    l_b_epi = l_b_endo + l_t_equator_b;
+    l_c_epi = l_c_endo + l_t_equator_c;
+
+    r_c_x = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.center_x"));
+    r_c_y = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.center_y"));
+    r_c_z = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.center_z"));
+
+    r_a_endo = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.a_endo"));
+    r_a_trunc = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.a_trunc"));
+    r_b_endo = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.b_endo"));
+    r_c_endo = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.c_endo"));
+
+    r_t_equator_b = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.t_equator_b"));
+    r_t_equator_c = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.t_equator_c"));
+    r_t_apex = boost::lexical_cast<double>(pt.get<std::string>("right_ventricle.t_apex"));
+
+    r_a_epi = r_a_endo + r_t_apex;
+    r_b_epi = r_b_endo + r_t_equator_b;
+    r_c_epi = r_c_endo + r_t_equator_c;
+
+}
 
 // Function
 FT ellipsoid (const Point& p,
@@ -89,14 +122,14 @@ FT ellipsoid (const Point& p,
 		a*a*b*b*(p.y() - o_y)*(p.y() - o_y)) - a*b*c;
 }
 
-FT left_ventricle (const Point& p)
+FT ventricles (const Point& p)
 {
-    if((ellipsoid(p, l_o_x, l_o_y, l_o_z, l_a_epi,  l_b_epi,  l_c_epi)  <= 0.0 &&
-	ellipsoid(p, l_o_x, l_o_y, l_o_z, l_a_endo, l_b_endo, l_c_endo) >= 0.0 &&
+    if((ellipsoid(p, l_c_x, l_c_y, l_c_z, l_a_epi,  l_b_epi,  l_c_epi)  <= 0.0 &&
+	ellipsoid(p, l_c_x, l_c_y, l_c_z, l_a_endo, l_b_endo, l_c_endo) >= 0.0 &&
 	p.z() <= l_a_trunc) ||
-       (ellipsoid(p, r_o_x, r_o_y, r_o_z, r_a_epi,  r_b_epi,  r_c_epi)  <= 0.0 &&
-	ellipsoid(p, r_o_x, r_o_y, r_o_z, r_a_endo, r_b_endo, r_c_endo) >= 0.0 &&
-	ellipsoid(p, l_o_x, l_o_y, l_o_z, l_a_endo, l_b_endo, l_c_endo) >  0.0 &&
+       (ellipsoid(p, r_c_x, r_c_y, r_c_z, r_a_epi,  r_b_epi,  r_c_epi)  <= 0.0 &&
+	ellipsoid(p, r_c_x, r_c_y, r_c_z, r_a_endo, r_b_endo, r_c_endo) >= 0.0 &&
+	ellipsoid(p, l_c_x, l_c_y, l_c_z, l_a_endo, l_b_endo, l_c_endo) >  0.0 &&
 	p.z() <= r_a_trunc))
 	return -1;
     else
@@ -107,23 +140,25 @@ FT left_ventricle (const Point& p)
 
 int main()
 {
-  // Domain
-  Mesh_domain domain(left_ventricle, K::Sphere_3(Point(0, 0, 0), 9.));
+    //Load paramters from file
+    load_parameters();
 
-  // Mesh criteria
-  double h = 0.2;
-  Mesh_criteria criteria(edge_size = h,
-                         facet_angle = 25, facet_size = h,
-                         cell_radius_edge_ratio = 2, cell_size = h);
+    // Domain
+    Mesh_domain domain(ventricles, K::Sphere_3(Point(0, 0, 0), 9.));
 
-  // Mesh generation without feature preservation
-  C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria,
-                                      CGAL::parameters::no_features());
+    // Mesh criteria
+    Mesh_criteria criteria(edge_size = h,
+    			   facet_angle = 25, facet_size = h,
+    			   cell_radius_edge_ratio = 2, cell_size = h);
 
-  std::ofstream medit_file("biventricle.mesh");
-  c3t3.output_to_medit(medit_file);
-  medit_file.close();
-  c3t3.clear();
+    // Mesh generation without feature preservation
+    C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria,
+    					CGAL::parameters::no_features());
 
-  return 0;
+    std::ofstream medit_file("biventricle.mesh");
+    c3t3.output_to_medit(medit_file);
+    medit_file.close();
+    c3t3.clear();
+
+    return 0;
 }
