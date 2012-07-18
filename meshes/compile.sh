f4e2e8a073171400873e268e3b7124e322366ade
Harish Narayanan <harish@simula.no>
2012-07-18 15:30:12 +0200

    Updated cgal code compile script

--- meshes/compile.sh
+++ meshes/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-g++-mp-4.4 -frounding-math -I/Users/harish/Work/FEniCS/dev/include  -c leftventricle.cpp
-g++-mp-4.4 -L/Users/harish/Work/FEniCS/dev/lib -lcgal -L/opt/local/lib -lmpfr -lgmp -lboost_thread-mt -frounding-math -o leftventricle leftventricle.o
+#g++-mp-4.6 -frounding-math -I/Users/harish/Work/FEniCS/dev/include  -c leftventricle.cpp
+#g++-mp-4.6 -L/Users/harish/Work/FEniCS/dev/lib -lcgal -L/opt/local/lib -lmpfr -lgmp -lboost_thread-mt -frounding-math -o leftventricle leftventricle.o
 
-#g++-mp-4.4 -frounding-math -I/Users/harish/Work/FEniCS/dev/include  -c biventricle.cpp
-#g++-mp-4.4 -L/Users/harish/Work/FEniCS/dev/lib -lcgal -L/opt/local/lib -lmpfr -lgmp -lboost_thread-mt -frounding-math -o biventricle biventricle.o
+g++-mp-4.6 -frounding-math -I/Users/harish/Work/FEniCS/dev/include  -c biventricle.cpp
+g++-mp-4.6 -L/Users/harish/Work/FEniCS/dev/lib -lcgal -L/opt/local/lib -lmpfr -lgmp -lboost_thread-mt -frounding-math -o biventricle biventricle.o
